1d538203cd8e0491ecaa0fedb97017de5da1f658
aswanik11 <aswanik11@gmail.com>
2018-04-10 01:17:59 +0530

    Angular-Material-Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+//Dev: Aswani Tewari
+import {Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'Angular Material';
+	showInfo(): void {
+		console.log("success");
+	}
+	breakpoint: number;
+	ngOnInit() {
+		this.breakpoint = (window.innerWidth <= 400) ? 1 : 4;
+		// this.breakpoint = (window.innerWidth <= 768) ? 2 : 3;
+	}
+
+	onResize(event) {
+		this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 4;
+		// this.breakpoint = (event.target.innerWidth <= 768) ? 2 : 3;
+	}
+	displayedColumns = ['in_inches', 'unit_qty', 'length', 'breadth', 'height'];
+	dataSource = ELEMENT_DATA;
+
 }
+
+export interface Element {
+	in_inches: string;
+	unit_qty: number;
+	length: number;
+	breadth: number;
+	height: number;
+}
+
+const ELEMENT_DATA: Element[] = [
+{in_inches: 'Item', unit_qty: 2, length: 10, breadth: 2, height: 10},
+{in_inches: 'Inner Pack Carton' , unit_qty: 2, length: 10, breadth: 2, height: 10},
+{in_inches: 'Master Pack Carton', unit_qty: 2, length: 10, breadth: 2, height: 10},
+{in_inches: 'Item Size', unit_qty: 2, length: 10, breadth: 2, height: 10},
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,107 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+//Dev: Aswani Tewari
+import {CdkTableModule} from '@angular/cdk/table';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {HttpModule} from '@angular/http';
+import {FileUploadModule} from 'primeng/primeng';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ]
+})
+export class DemoMaterialModule {}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    FileUploadModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: []
 })
-export class AppModule { }
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
